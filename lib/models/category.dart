4d9b468db5a8e69644a3fdd31f8540f729d9dc38
Hawa Nizamani <hawanizamani17@gmail.com>
2025-08-11 22:41:06 +0500

    Clean up ignored files and track only main files

--- lib/models/category.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class Category {
-  final String name;
-  final Color color;
-  final String imagePath;
-
-  Category({required this.name, required this.color, required this.imagePath});
-}
